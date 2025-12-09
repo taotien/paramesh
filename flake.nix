7f6b8fb1a580943b4f2c003b39d291af7ce65a89
Tao Tien <29749622+taotien@users.noreply.github.com>
2025-12-08 20:29:45 -0800

    flake deps

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+    inputs = {
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+    outputs = {
+        self,
+        nixpkgs,
+        flake-utils,
+    }:
+        flake-utils.lib.eachDefaultSystem (
+            system: let
+                pkgs = nixpkgs.legacyPackages.${system};
+            in
+                with pkgs; let
+                    microcad = {};
+                in {
+                    devShell = pkgs.mkShell rec {
+                        buildInputs = [
+                            meshlab
+
+                            just
+
+                            nasm
+
+                            pkg-config
+                            ninja
+                            cmake
+                            stdenv.cc.cc
+
+                            # microcad-bevy
+                            vulkan-loader
+                            vulkan-tools
+                            xorg.libX11
+                            xorg.libXcursor
+                            xorg.libXi
+                            xorg.libXrandr
+                            libxkbcommon
+                        ];
+                        LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+                    };
+                }
+        );
+}
